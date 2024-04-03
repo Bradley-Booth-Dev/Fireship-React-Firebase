a738b8182b5076b8ef6838da30306ed1fdd8fe8a
Bradley Booth <123832311+Bradley-Booth-Dev@users.noreply.github.com>
2024-04-03 12:22:23 +0100

    Delete fireship directory

--- fireship/components/Loader.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Loader({ show }: { show: boolean }) {
-  return show ? <div className="loader"></div> : null;
-}
--- fireship/pages/admin/[slug].tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function AdminPostsPage({}) {
-  return (
-    <main>
-      <h1>SLUG</h1>
-    </main>
-  );
-}
